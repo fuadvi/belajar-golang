56a1aa42896a61fa85a6dedd1180e2e6e13ee493
fuad <fuad.pcd@gmail.com>
2022-09-03 00:21:02 +0700

    switch tanpa kondisi

--- hari kelima/switch.go
+++ hari kelima/switch.go
@@ -16,11 +16,23 @@ func main() {
 		fmt.Println("Hi boleh kenalan ?")
 	}
 
-	switch lenght := len(name); lenght > 5 {
-	case true:
-		fmt.Println("nama Terlalu Panjang")
-	case false:
-		fmt.Println("nama sudah benar")
+	// short hand switch expresseion
+	// switch lenght := len(name); lenght > 5 {
+	// case true:
+	// 	fmt.Println("nama Terlalu Panjang")
+	// case false:
+	// 	fmt.Println("nama sudah benar")
+	// }
+
+	// switch tanpa kondisi
+	lenght := len(name)
+	switch {
+	case lenght > 10:
+		fmt.Println("nama terlalu panjang")
+	case lenght > 5:
+		fmt.Println("nama lumayan panjang")
+	default:
+		fmt.Println("nama sudah sesuai")
 	}
 
 }
