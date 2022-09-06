90500b3790eb684dcb4137c786aeb62687e629b4
fuad <fuad.pcd@gmail.com>
2022-09-06 21:10:21 +0700

    interface kosong

---
+++ hari kedelapan/interface-kosong.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func ups(i int) interface{} {
+	if i == 1 {
+		return 1
+	} else if i == 2 {
+		return true
+	} else {
+		return "ups"
+	}
+}
+
+func main() {
+	data := ups(3)
+	fmt.Println(data)
+}
